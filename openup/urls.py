748f133407d098abbcdf1ab6a184ffab6eb026de
Nicolas Noé <nicolas@niconoe.org>
2012-07-17 15:02:55 +0200

    Basic structure of URLs/Apps/Views/Templates.

--- openup/urls.py
+++ openup/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'openup.views.home', name='home'),
-    # url(r'^openup/', include('openup.foo.urls')),
+    url(r'^', include('website.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- website/views.py
+++ website/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def my_render(template_path, request_obj, context={}):
+    return render_to_response(template_path, context, context_instance=RequestContext(request_obj))
+
+def index(request):
+    return my_render('index.html', request)
+    
+    
+    

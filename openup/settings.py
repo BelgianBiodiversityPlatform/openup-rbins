37ab796cf02bc0d212259a704d628f641c9fed94
Nicolas Noé <nicolas@niconoe.org>
2012-07-17 14:37:45 +0200

    Activated two apps: website (empty) and the Admin app.

--- openup/settings.py
+++ openup/settings.py
@@ -104,13 +104,14 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'website'
 )
 
 # A sample logging configuration. The only tangible logging
